ce926c1041c27d22e473f70369b3557344e7b0fb
vonopr <vonopr@ya.ru>
2021-08-08 13:02:01 +0300

    Adjust css-styles

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,7 @@ html_theme = 'karma_sphinx_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -154,4 +154,6 @@ man_pages = [
 programoutput_prompt_template = "{command}"#"{command}\n{output}"
 
 
+html_static_path = ['_static']
+html_css_files = [ 'custom.css' ]
 html_sidebars = { '**': ['localtoc.html', 'relations.html'] }
