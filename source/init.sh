9073243eccf0a90a95b4cab3f97ca50e379deea5
vonopr <vonopr@ya.ru>
2021-08-08 15:16:55 +0300

    Hide gitlab access token

--- source/init.sh
+++ source/init.sh
@@ -1,5 +1,5 @@
 mkdir -p data
-curl -o example-data.zip --header 'Private-Token: yiuskGfW5Z31io2bmwgh'  http://tesla.parallel.ru/api/v4/projects/145/packages/generic/example-data/0.1.0/example-data.zip
+curl -o example-data.zip --header "Private-Token: $GITLAB_TOKEN"   http://tesla.parallel.ru/api/v4/projects/145/packages/generic/example-data/0.1.0/example-data.zip
 unzip example-data.zip
 mv -u example-data/* data/
 mkdir -p pics
